ec014b4a902eb9e2d0754fd11748463835b9b193
tikripman <131655046+tikripman@users.noreply.github.com>
2023-04-26 09:02:19 +0800

    Create define.h
    create this define

---
+++ define.h
@@ -0,0 +1,8 @@
+#ifndef define_h
+#define define_h
+
+#define THREAD_SYNC  1
+#define MAX_COUNTS_OF_THREADS  200
+#define PER_COUNTS_OF_THREADS_PER_DAY  32
+#define MAX_API_PER_THREAD  10
+#define support_mulit ON
