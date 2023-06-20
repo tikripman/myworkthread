a615ba1efc180b999139256d27f1499370b22d3a
tikripman <131655046+tikripman@users.noreply.github.com>
2023-06-20 11:05:45 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ComboExDemo.rc
+//
+#define IDD_COMBOEXDEMO_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_COMBO                       1000
+#define IDC_COMBO1                      1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
